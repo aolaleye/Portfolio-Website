19a2d02daf28a4266ab00a3e53c23e7780eb0886
aolaleye <30451506+aolaleye@users.noreply.github.com>
2022-01-27 05:49:26 -0500

    update content

--- js/app.js
+++ js/app.js
@@ -3,95 +3,132 @@ $(document).ready(function(){
     const mq = window.matchMedia( "(min-width: 1300px)" );
 
     //--- Category Filter on Portfolio Page ---//
-    $(".dev-category").css({"background": "#189dac"});
-    $(".dev-category").css({"color": "white"});
+    $(".personal-category").css({"background": "#189dac"});
+    $(".personal-category").css({"color": "white"});
 
-    $(".ux-category").hover(function(){
+    $(".access-password").val('');
+    $(".access-form").hide();
+    $(".ux").hide();
+    $(".dev").show();
+    $(".back-to-top").show();
+
+    $(".professional-category").hover(function(){
         $(".lock-icon").css({"filter": "brightness(0) invert(1)"});
         }, function(){
-        $(".lock-icon").css({"filter": "none"});
+        // $(".lock-icon").css({"filter": "none"});
     });
 
     var category = ['dev', 'ux', 'ui']
     var p = 0;
 
-    $.each(category, function(p, val) {
-
-        $("." + category[p] + "-category").on("click", function() {
-
-            for (j = 0; j < category.length; j++) {
-                $("." + category[j] + "-category").css({"background": "rgba(123, 122, 122, 0.4)"});
-                $("." + category[j] + "-category").css({"color": "#3e3e3e"});
-                $(".lock-icon").css({"filter": "none"});
-
-                $("." + category[j]).hide();
-
-                var selected = category[p];
-            
-                if (selected !== 'ui') {
-                    $("." + selected).show();
-                    $("." + selected + "-category").css({"background": "#189dac"});
-                    $("." + selected + "-category").css({"color": "white"});
-                    $(".lock-icon").css({"filter": "brightness(0) invert(1)"});
-                    $(".ux-category").hover(function(){
-                        $(".lock-icon").css({"filter": "brightness(0) invert(1)"});
-                    });
-                }
-
-                if (selected === 'dev') {
-                    $(".lock-icon").css({"filter": "none"});
-                    $(".ux-category").hover(function(){
-                        $(".lock-icon").css({"filter": "brightness(0) invert(1)"});
-                        }, function(){
-                        $(".lock-icon").css({"filter": "none"});
-                    });
-                }
-
-                if (mq.matches) {
-
-                    var hovered = false;
-                
-                    $("." + category[j] + "-category").hover(
-                        function() {
-                            if (hovered) {
-                                $(this).animate({
-                                backgroundColor: "#56b6c1",
-                                color: 'white',
-                                }, 1);
-                                $(".lock-icon").css({"filter": "brightness(0) invert(1)"});           
-                            } else {
-                                $(this).animate({
-                                backgroundColor: "rgba(123, 122, 122, 0.4)",
-                                color: '#3e3e3e',
-                                }, .1 );
-                                $(".lock-icon").css({"filter": "none"});                                
-                            }
-                            hovered = !hovered;
-                        }
-                        
-                    );
-
-                    $("." + selected + "-category").hover(
-                        function() {
-                            $(this).animate({
-                                backgroundColor: "#189dac",
-                                color: 'white',
-                                }, .1);
-                            // $(".lock-icon").css({"filter": "brightness(0) invert(1)"});
-                        }
-                    );
-                }
-            }
-            
-        });
-
-        $(".client-access-button").click(function() {
-            if ($( ".access-password" ).val() !== 'pasunchftrywps,&&&&sdfbva=0-3i50983y5') {
-                $(".error-message").show();
-            }
-        });
-
+    $(".personal-category").on("click", function() {
+        $(".ux").hide();
+        $(".access-form").hide();
+        $(".back-to-top").show();
+        $(".dev").show();
+        $(".access-password").val('');
+        $(".personal-category").css({"background": "#189dac"});
+        $(".personal-category").css({"color": "white"});
+        $(".professional-category").css({"background": "rgba(123, 122, 122, 0.4)"});
+        $(".professional-category").css({"color": "#3e3e3e"});
+        $(".lock-icon").css({"filter": "none"});
     });
+
+    $(".professional-category").on("click", function() {
+        $(".ux").hide();
+        $(".dev").hide();
+        $(".access-password").val('');
+        $(".access-form").show();
+        $(".back-to-top").hide();
+        $(".error-message").hide();
+        $(".professional-category").css({"background": "#189dac"});
+        $(".professional-category").css({"color": "white"});
+        $(".lock-icon").css({"filter": "brightness(0) invert(1)"});
+        $(".personal-category").css({"background": "rgba(123, 122, 122, 0.4)"});
+        $(".personal-category").css({"color": "#3e3e3e"});
+    });
+
+    $(".client-access-button").on("click", function() {       
+        if ($(".access-password").val() === 'd8nb6p') {
+            $(".access-form").hide();
+            $(".ux").show();
+            $(".back-to-top").show();
+        } else {
+            $(".error-message").show();
+        }
+    });
+
+    // $.each(category, function(p, val) {
+
+    //     $("." + category[p] + "-category").on("click", function() {
+
+    //         for (j = 0; j < category.length; j++) {
+    //             $("." + category[j] + "-category").css({"background": "rgba(123, 122, 122, 0.4)"});
+    //             $("." + category[j] + "-category").css({"color": "#3e3e3e"});
+    //             $(".lock-icon").css({"filter": "none"});
+
+    //             $("." + category[j]).hide();
+
+    //             var selected = category[p];
+            
+    //             if (selected !== 'ui') {
+    //                 $("." + selected).show();
+    //                 $("." + selected + "-category").css({"background": "#189dac"});
+    //                 $("." + selected + "-category").css({"color": "white"});
+    //                 $(".lock-icon").css({"filter": "brightness(0) invert(1)"});
+    //                 $(".ux-category").hover(function(){
+    //                     $(".lock-icon").css({"filter": "brightness(0) invert(1)"});
+    //                 });
+    //             }
+
+    //             if (selected === 'dev') {
+    //                 $(".lock-icon").css({"filter": "none"});
+    //                 $(".ux-category").hover(function(){
+    //                     $(".lock-icon").css({"filter": "brightness(0) invert(1)"});
+    //                     }, function(){
+    //                     $(".lock-icon").css({"filter": "none"});
+    //                 });
+    //             }
+
+    //             if (mq.matches) {
+
+    //                 var hovered = false;
+                
+    //                 $("." + category[j] + "-category").hover(
+    //                     function() {
+    //                         if (hovered) {
+    //                             $(this).animate({
+    //                             backgroundColor: "#56b6c1",
+    //                             color: 'white',
+    //                             }, 1);
+    //                             $(".lock-icon").css({"filter": "brightness(0) invert(1)"});           
+    //                         } else {
+    //                             $(this).animate({
+    //                             backgroundColor: "rgba(123, 122, 122, 0.4)",
+    //                             color: '#3e3e3e',
+    //                             }, .1 );
+    //                             $(".lock-icon").css({"filter": "none"});                                
+    //                         }
+    //                         hovered = !hovered;
+    //                     }
+                        
+    //                 );
+
+    //                 $("." + selected + "-category").hover(
+    //                     function() {
+    //                         $(this).animate({
+    //                             backgroundColor: "#189dac",
+    //                             color: 'white',
+    //                             }, .1);
+    //                         // $(".lock-icon").css({"filter": "brightness(0) invert(1)"});
+    //                     }
+    //                 );
+    //             }
+    //         }
+            
+    //     });
+
+    // });
 
 
     //--- Reveal Langagues on Hover ---//
